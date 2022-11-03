3a5d14c788b8168df43d9b7fa2af965ce19b46db
shroukkhaled99 <shroukkhaled381@gmail.com>
2022-11-03 12:22:50 +0200

    updated

--- js/AyaMedica.js
+++ js/AyaMedica.js
@@ -336,9 +336,9 @@ if (document.title === "Events") {
         {
             imgPath: "images/drassem.jpg",
             name: "Assem Zahran",
-            clinicName: "Ophthanlmology Consultant",
+            clinicName: "International Femto Lasik Centre - Zahran Eye Center",
             link: "https://youtu.be/T0qQ21D2_fg",
-            location: "International femto lasik centre - new cairo / Zahran eye center - Damiette",
+            location: "New Cairo - Damiette",
             description: `Dr. Assem Zahran is a consultant ophthalmologist who provides the most advanced refractive eye procedures for vision correction, leveraging the centers' cutting-edge technology. Dr. Assem has over 20 years of experience in this field.\n
             Dr. Assem Zahran is Cataract and low vision surgeries consultant, He invented the keratoconus lift linked with cataract surgery, as well as the procedure for correcting astigmatism associated with cataracts. Dr. Zahran is the inventor of the present theory of lateral strabismus and the discoverer of the method of treating lateral strabismus without surgery. He discovered seven new migraine triggers related to eye muscles. He was awarded International certifications of excellence for obtaining the greatest global rates in femto lasik, LASIK SPK, and multifocal lens implants. Furthermore, he used femtolaser technology to execute seven new operations for the first time in the world. He developed the circular fragmentation approach to cure fossilized cataracts. Dr. Zahran was the first in the world to implant a second secondary lens. He is the director of the International Femtolasik Center (IFLC) The International Femtolasik Center (IFLC)which is one of nine worldwide accredited centers in femto smile and femto lasik surgeries.`,
             packageIncludes: [
@@ -348,7 +348,7 @@ if (document.title === "Events") {
             ],
             locations: [
                 {
-                    name: "International femto lasik centre - new cairo",
+                    name: "International Femto Lasik Centre - New Cairo",
                     link: "https://triumphhotel.com/luxury-home",
                     package: [
                         "Accommodation at Truimph Hotel / 3 nights accommodation on bb basis sgl room .",
@@ -360,7 +360,7 @@ if (document.title === "Events") {
                     foreign_OverDay: 170
                 },
                 {
-                    name: "Zahran eye center - Damiette",
+                    name: "Zahran Eye Center - Damiette",
                     link: "https://www.steigenberger.com/en/hotels/all-hotels/egypt/damietta/steigenberger-hotel-el-lessan",
                     package: [
                         "Accommodation at Steigenberger El Lesan  / 3 nights accommodation on bb basis sgl room.",
@@ -377,9 +377,9 @@ if (document.title === "Events") {
         {
             imgPath: "images/drmagdy.jpg",
             name: "Magdy Khalaf",
-            clinicName: "i-vision",
+            clinicName: "I-Vision",
             link: "https://youtu.be/gHczffuexMg",
-            location: "i-vision - heliopolis",
+            location: "Heliopolis, Cairo",
             description: `Professor of Ophthalmology at Al-Azhar University's Faculty of Medicine University of Amsterdam Fellow, Netherlands University of Regensburg - Germany Fellow Member of the European Society of Cataract and Refractive Surgery and the American Society of Cataract and Refractive Surgery.\n
             Dr. Magdy Khallaf is a corneal, vision correction, and keratoconus surgery consultant. He holds fellowships at the University of Amsterdam and the University of Regensburg in Germany. In addition, he is an honorary president of the International Society of Cataract Surgery. He won an award and a letter of appreciation from the World Eye Conference in Hong Kong for the best study on the treatment of corneal transplant complications, and he has several worldwide keratoconus studies. He is also a corneal and keratoconus surgery consultant at i-vision Eye Hospital in Heliopolis.
             `,
@@ -390,7 +390,7 @@ if (document.title === "Events") {
             ],
             locations: [
                 {
-                    name: "i-vision - heliopolis ",
+                    name: "I-Vision - Heliopolis ",
                     link: "https://triumphhotel.com/luxury-home",
                     package: [
                         "Accommodation at Truimph Hotel / 3 nights accommodation on bb basis sgl room .",
@@ -408,9 +408,9 @@ if (document.title === "Events") {
         {
             imgPath: "images/drahmedelmassry.png",
             name: "Ahmed Elmassry",
-            clinicName: "alex vision center",
+            clinicName: "Alex Vision Center",
             link: "https://youtu.be/D1S-oRz9Mho",
-            location: "alex vision center",
+            location: "Alexandria",
             description: `Dr. Mahmoud Ismail, Professor of Ophthalmology and Head of Ophthalmology at Al-Azhar University, is also a Professor of Ophthalmology at the University of Alicante in Spain.\n
             He is the medical director of Nour Al-Hayah Hospital in Egypt. Moreover, he is the first and only non-Spanish doctor to receive the Spanish National Research Award on a global scale. He is an innovator of the International Innovation Award for Postal Lens Implantation for the Treatment of Ulcers, requiring one horn procedure for three surgeries. Dr. Ismail is a Member of the International Committee of Experts, the Court for Research of the European Association for White Water and Visual Defects, and the European Association for White Water and Visual Defects. He has an annual course at the American Academy conferences in his honor. He is also one of the worldwide doctors that established laser surgery guidelines in the nineties.
             `,
@@ -421,7 +421,7 @@ if (document.title === "Events") {
             ],
             locations: [
                 {
-                    name: "alex vision center ",
+                    name: "Alex Vision Center ",
                     link: "https://www.marriott.com/en-us/hotels/alysi-sheraton-montazah-hotel/overview/?scid=f2ae0541-1279-4f24-b197-a979c79310b0",
                     package: [
                         "Accommodation at  Sheraton Montazah Hotel / 3 nights accommodation on bb basis sgl room .",
@@ -439,7 +439,7 @@ if (document.title === "Events") {
             name: "Mahmoud Ismail",
             clinicName: "Nour Al-Hayah Eye Center",
             link: "https://youtu.be/D1S-oRz9Mho",
-            location: "Heliopolis - Cairo",
+            location: "Heliopolis, Cairo",
             description: `Dr. Mahmoud Ismail, Professor of Ophthalmology and Head of Ophthalmology at Al-Azhar University, is also a Professor of Ophthalmology at the University of Alicante in Spain. He is the medical director of Nour Al-Hayah Hospital in Egypt.\n
              Moreover, he is the first and only non-Spanish doctor to receive the Spanish National Research Award on a global scale. He is an innovator of the International Innovation Award for Postal Lens Implantation for the Treatment of Ulcers, requiring one horn procedure for three surgeries. Dr. Ismail is a Member of the International Committee of Experts, the Court for Research of the European Association for White Water and Visual Defects, and the European Association for White Water and Visual Defects. He has an annual course at the American Academy conferences in his honor. He is also one of the worldwide doctors that established laser surgery guidelines in the nineties.`,
             packageIncludes: [
@@ -449,7 +449,7 @@ if (document.title === "Events") {
             ],
             locations: [
                 {
-                    name: "Nour Al-Hayah - heliopolis ",
+                    name: "Nour Al-Hayah - Heliopolis ",
                     link: "https://triumphhotel.com/luxury-home",
                     package: [
                         "Accommodation at Truimph Hotel / 3 nights accommodation on bb basis sgl room .",
